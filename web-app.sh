2381a56fed4075519c0c7939d7e7d8cff79ac401
jcraney143 <jacob.craney@mail.bcu.ac.uk>
2024-01-19 13:10:24 +0000

    Changed port from 8080 to 5000.

--- web-app.sh
+++ web-app.sh
@@ -9,20 +9,16 @@ cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/. 
 
 echo "FROM python" >> tempdir/Dockerfile 
-
 echo "RUN pip install flask" >> tempdir/Dockerfile
-
 echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
 echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
 echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile 
-
 echo "EXPOSE 5000" >> tempdir/Dockerfile 
-
 echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile 
 
 cd tempdir
-sudo docker build -t sampleapp . 
 
-sudo docker run -t -d -p 5000:5000 --name samplerunning sampleapp
+docker build -t sampleapp . 
 
-sudo docker ps -a
+docker run -t -d -p 5000:5000 --name samplerunning sampleapp
+docker ps -a
